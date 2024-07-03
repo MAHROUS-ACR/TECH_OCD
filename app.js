b4811aa228f399d69a88e25b614b6792d62aa5f4
MAHROUS-ACR <mahrous.ref@gmail.com>
2024-07-03 11:51:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+     apiKey: "AIzaSyACFHbMr19jQI8t39FvABCSuMpSz6TR5hY",
+  authDomain: "tch-o-5e243.firebaseapp.com",
+  databaseURL: "https://tch-o-5e243-default-rtdb.firebaseio.com",
+  projectId: "tch-o-5e243",
+  storageBucket: "tch-o-5e243.appspot.com",
+  messagingSenderId: "356977983925",
+  appId: "1:356977983925:web:0e8669180f9d664a49d9fc",
+  measurementId: "G-9X82V7SXPX"
 };
 
 // Initialize Firebase
